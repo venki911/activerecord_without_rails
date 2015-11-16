978980884c2a0ef0bb0c8d4b5caab2dc7ac72662
Fran Martinez <martinezcoder@gmail.com>
2015-11-16 11:28:43 +0100

    lists users

--- app.rb
+++ app.rb
@@ -9,3 +9,13 @@ ActiveRecord::Base.establish_connection(configuration['development'])
 class User < ActiveRecord::Base
 
 end
+
+u1 = User.new(name: "Juan")
+u1.save
+u2 = User.new(name: "Pepe")
+u2.save
+puts "Number of users: #{User.count}"
+User.all.each do |user|
+  puts user.inspect
+end
+
